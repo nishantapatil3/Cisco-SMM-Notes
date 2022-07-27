19498b811255884dd928df597a2f61a21e952922
nishantapatil3 <nishant.apatil3@gmail.com>
2022-07-27 07:05:47 -0700

    Update k3d start script

--- k3d/start_k3d.sh
+++ k3d/start_k3d.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+export KUBECONFIG=~/Downloads/k3d-1.yaml
 k3d cluster create --config ~/kindconfig/k3d/k3d_config.yaml
+k3d kubeconfig get k3d-1 > ~/Downloads/k3d-1.yaml
+
+echo "\n\nexport KUBECONFIG=~/Downloads/k3d-1.yaml"
