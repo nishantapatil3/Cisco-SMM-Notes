2cc370cdbbafd3b481edceb0eec73fdf3b345f5d
Nishant Patil <nishant.apatil3@gmail.com>
2023-11-10 21:40:00 -0800

    update kind config

--- kind/start_kind.sh
+++ kind/start_kind.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-KINDCONFIG="$HOME/kindconfig/kind/kind_config.yaml"
-KUBECONFIG="$HOME/Downloads/kind-1.yaml"
+KINDCONFIG="$HOME/kind/kind_config.yaml"
+KUBECONFIG="$HOME/kind/cluster1.yaml"
 
 kind create cluster --config $KINDCONFIG
 kind get kubeconfig --name kind-1 > $KUBECONFIG
 echo "kind-1 Kubeconfig generated - $KUBECONFIG"
 
-export KUBECONFIG=$KUBECONFIG
+echo "export KUBECONFIG=$KUBECONFIG"
